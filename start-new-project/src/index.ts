a4bd9b24c7955e4e0d4a81394aaab39f99dfce98
Ilya Kozlovski <kozlvskiilya@gmail.com>
2025-03-31 15:53:10 +0300

    test Middleware

--- start-new-project/src/index.ts
+++ start-new-project/src/index.ts
@@ -1,9 +1,36 @@
-import express, {Request, Response} from 'express'
+import express, {NextFunction, Request, Response} from 'express'
 import bodyParser from 'body-parser'
 import {productsRoute} from "./routes/products-router";
 import {addressesRoute} from "./routes/addresses-router";
 
 const app = express()
+
+
+// const blablaMiddleware = (req: Request, res: Response, next: NextFunction) => {
+//     // @ts-ignore
+//     req.blabla = 'hello'
+//     next()
+// };
+//
+// const authGuardMiddleware = (req: Request, res: Response, next: NextFunction) => {
+//     // @ts-ignore
+//     if(req.query.token  === '123'){
+//         next()
+//     } else {
+//         res.send(401)
+//     }
+// };
+//
+// let requestCounter = 0
+//
+// const requestCounterMiddleware = (req: Request, res: Response, next: NextFunction) => {
+//     requestCounter++
+//     next()
+// };
+// app.use(requestCounterMiddleware)
+// app.use(blablaMiddleware)
+// app.use(authGuardMiddleware)
+
 
 const port = process.env.PORT || 5000
 
@@ -13,6 +40,19 @@ app.use(parserMiddleware)
 
 
 
+// app.get('/products', (req: Request, res: Response) => {
+//     // @ts-ignore
+//     const blabla = req.blabla
+//     res.send({value: blabla + "!!!!!!!" } )
+//
+// });
+//
+// app.get('/products',(req: Request, res: Response) => {
+//     // @ts-ignore
+//     const blabla = req.blabla
+//     res.send({value: blabla + "from users!!!!!!!" } )
+//
+// });
 
 app.use('/products' , productsRoute)
 app.use('/addresses' , addressesRoute)
